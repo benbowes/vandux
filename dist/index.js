ab81a9fa3af302d544e9e51f04e7324528f02b1b
Ben Bowes <benbowes@factor5.com.au>
2017-12-14 00:13:55 +1100

    Build demo

--- dist/index.js
+++ dist/index.js
@@ -68,310 +68,55 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _stateMachine = __webpack_require__(4);
-
-var _stateMachine2 = _interopRequireDefault(_stateMachine);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/**
-* creatStore() is the initializer for the store
-* @param {function} reducer - see addThingsReducer
-* @param {object} initialState - whatever you want to intialise your
-* store with - this part of state will sit under th name of the
-* function in the object tree.
-*/
-exports.default = function (_ref) {
-  var reducer = _ref.reducer,
-      initialState = _ref.initialState;
-
-  var context = {};
-
-  context.pubSub = function () {
-    var store = (0, _stateMachine2.default)();
-    var events = {};
-
-    return {
-      getState: function getState() {
-        return store.state;
-      },
-
-      subscribe: function subscribe(event, listener) {
-        if (!events.hasOwnProperty.call(events, event)) events[event] = [];
-        var index = events[event].push(listener) - 1;
-        return {
-          unsubscribe: function unsubscribe() {
-            return delete events[event][index];
-          }
-        };
-      },
-
-      connect: function connect(eventsToSubscribe) {
-        return function (dom) {
-          return function (renderFunction) {
-            eventsToSubscribe.forEach(function (evt) {
-              context.instance.subscribe(evt, function (obj) {
-                if (renderFunction) renderFunction(obj, dom, evt);
-              });
-              context.instance.publish(evt, reducer(initialState, { type: 'INIT' }));
-            });
-            return context.instance;
-          };
-        };
-      },
-
-      publish: function publish(event, payload) {
-        if (!events.hasOwnProperty.call(events, event)) return;
-        store.setState(reducer(store.state, { type: event, data: payload }));
-        events[event].forEach(function (listener) {
-          listener(store.state !== undefined ? store.state : {});
-        });
-        // Print each published message if location has '?debug'
-        if (window.location.search.indexOf('debug') > -1) console.log(event, payload);
-      }
-    };
-  };
-
-  context.instance = context.pubSub();
-
-  return context.instance;
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _stateMachine = __webpack_require__(4);\n\nvar _stateMachine2 = _interopRequireDefault(_stateMachine);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n/**\n* creatStore() is the initializer for the store\n* @param {function} reducer - see addThingsReducer\n* @param {object} initialState - whatever you want to intialise your\n* store with - this part of state will sit under th name of the\n* function in the object tree.\n*/\nexports.default = function (_ref) {\n  var reducer = _ref.reducer,\n      initialState = _ref.initialState;\n\n  var context = {};\n\n  context.makeStore = function () {\n    var store = (0, _stateMachine2.default)();\n    var events = {};\n    var doInitialRender = function doInitialRender(renderFunction, dom) {\n      var newState = reducer(initialState, { type: 'INIT' });\n      store.setState(newState);\n      if (renderFunction) renderFunction(newState, dom, 'INIT', context.createdStore);\n    };\n\n    return {\n      getState: function getState() {\n        return store.state;\n      },\n      subscribe: function subscribe(event, listener) {\n        if (!events.hasOwnProperty.call(events, event)) events[event] = [];\n        var index = events[event].push(listener) - 1;\n        return { unsubscribe: function unsubscribe() {\n            return delete events[event][index];\n          } };\n      },\n      connect: function connect(eventsToSubscribe) {\n        return function (dom) {\n          return function (renderFunction) {\n            eventsToSubscribe.forEach(function (evt) {\n              context.createdStore.subscribe(evt, function (obj) {\n                if (renderFunction) renderFunction(obj, dom, evt, context.createdStore);\n              });\n            });\n            doInitialRender(renderFunction, dom);\n            return context.createdStore;\n          };\n        };\n      },\n      publish: function publish(event, payload) {\n        if (!events.hasOwnProperty.call(events, event)) return;\n        store.setState(reducer(store.state, { type: event, data: payload }));\n        events[event].forEach(function (listener) {\n          listener(store.state !== undefined ? store.state : {});\n        });\n        // Print each published message if location has '?debug'\n        if (window.location.search.indexOf('debug') > -1) console.log(event, payload);\n      }\n    };\n  };\n\n  context.createdStore = context.makeStore();\n\n  return context.createdStore;\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/lib/createStore.js\n// module id = 0\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/lib/createStore.js?");
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(2);
-
+eval("module.exports = __webpack_require__(2);\n\n\n//////////////////\n// WEBPACK FOOTER\n// multi ./src/index.js\n// module id = 1\n// module chunks = 0\n\n//# sourceURL=webpack:///multi_./src/index.js?");
 
 /***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-var _componentA = __webpack_require__(3);
-
-var _componentA2 = _interopRequireDefault(_componentA);
-
-var _componentB = __webpack_require__(6);
-
-var _componentB2 = _interopRequireDefault(_componentB);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-document.addEventListener('DOMContentLoaded', function () {
-  (0, _componentA2.default)({ thing: 'ddd', aaa: '' });
-  (0, _componentB2.default)({ a_bit_of: 'initial state', bbb: '', value: 20 });
-});
+eval("\n\nvar _componentA = __webpack_require__(3);\n\nvar _componentA2 = _interopRequireDefault(_componentA);\n\nvar _componentB = __webpack_require__(6);\n\nvar _componentB2 = _interopRequireDefault(_componentB);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\ndocument.addEventListener('DOMContentLoaded', function () {\n  (0, _componentA2.default)({ name: '' });\n  (0, _componentB2.default)({ title: '', value: 20 });\n});\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/index.js\n// module id = 2\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _createStore = __webpack_require__(0);
-
-var _createStore2 = _interopRequireDefault(_createStore);
-
-var _reducer = __webpack_require__(5);
-
-var _reducer2 = _interopRequireDefault(_reducer);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// Perform DOM alterations in here
-function render(obj, dom, event) {
-  var el = dom;
-  el.innerText = JSON.stringify(_extends({}, obj, { lastEvent: event }), null, 2);
-  el.classList.remove('added-data');
-  setTimeout(function () {
-    return el.classList.add('added-data');
-  });
-  el.setAttribute('data-last-event-fired', event);
-}
-
-// Setup connections in here
-function setupListeners(store) {
-  document.querySelector('#inputAAA').addEventListener('keyup', function (e) {
-    store.publish('ADD_IT_AS_IT_COMES', { aaa: e.target.value });
-  });
-}
-
-// Entry function
-
-exports.default = function (initialState) {
-  var dom = document.querySelector('#connected1');
-  var store = (0, _createStore2.default)({
-    reducer: _reducer2.default,
-    initialState: initialState
-  }).connect(['ADD_IT_AS_IT_COMES'])(dom)(render);
-
-  setupListeners(store);
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };\n\nvar _createStore = __webpack_require__(0);\n\nvar _createStore2 = _interopRequireDefault(_createStore);\n\nvar _reducer = __webpack_require__(5);\n\nvar _reducer2 = _interopRequireDefault(_reducer);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// Perform DOM alterations in here\nfunction render(obj, el, event) {\n  var codeBlockEl = el.querySelector('[data-id=code]');\n  var lastEventEl = el.querySelector('[data-id=last-event]');\n  var nameEl = el.querySelector('[data-id=name]');\n\n  lastEventEl.innerText = event;\n  nameEl.innerText = obj.name;\n  codeBlockEl.innerText = JSON.stringify(_extends({}, obj, { lastEvent: event }), null, 2);\n}\n\nfunction addListeners(el, store) {\n  el.querySelector('[data-id=input]').addEventListener('keyup', function (e) {\n    store.publish('SMASH_IT_IN_THERE', { name: e.target.value });\n  }, true);\n}\n\n// Entry function\n\nexports.default = function (initialState) {\n  var el = document.querySelector('[data-id=componentA');\n  var store = (0, _createStore2.default)({\n    reducer: _reducer2.default,\n    initialState: initialState\n  }).connect(['SMASH_IT_IN_THERE'])(el)(render);\n\n  addListeners(el, store);\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/components/componentA/index.js\n// module id = 3\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/components/componentA/index.js?");
 
 /***/ }),
 /* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-/**
-* State Machine Pattern
-* A function that takes initialState and returns
-* an interface for altering/getting state
-*/
-exports.default = function () {
-  var initialState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  var state = initialState;
-  var setState = function setState(newState) {
-    state = _extends({}, state, newState);
-  };
-  return {
-    setState: setState,
-    get state() {
-      return state;
-    }
-  };
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };\n\n/**\n* State Machine Pattern\n* A function that takes initialState and returns\n* an interface for altering/getting state\n*/\nexports.default = function () {\n  var initialState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};\n\n  var state = initialState;\n  var setState = function setState(newState) {\n    state = _extends({}, state, newState);\n  };\n  return {\n    setState: setState,\n    get state() {\n      return state;\n    }\n  };\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/lib/stateMachine.js\n// module id = 4\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/lib/stateMachine.js?");
 
 /***/ }),
 /* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-// Perform STATE manipulations in here
-exports.default = function () {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  var action = arguments[1];
-
-  switch (action.type) {
-    case 'ADD_IT_AS_IT_COMES':
-      return _extends({}, state, action.data);
-    default:
-      return _extends({}, state);
-  }
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };\n\n// Perform STATE manipulations in here\nexports.default = function () {\n  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};\n  var action = arguments[1];\n\n  switch (action.type) {\n    case 'SMASH_IT_IN_THERE':\n      return _extends({}, state, action.data);\n    default:\n      return _extends({}, state);\n  }\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/components/componentA/reducer.js\n// module id = 5\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/components/componentA/reducer.js?");
 
 /***/ }),
 /* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _createStore = __webpack_require__(0);
-
-var _createStore2 = _interopRequireDefault(_createStore);
-
-var _reducer = __webpack_require__(7);
-
-var _reducer2 = _interopRequireDefault(_reducer);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// Perform DOM alterations in here
-function render(obj, dom, event) {
-  var el = dom;
-  el.innerText = JSON.stringify(_extends({}, obj, { lastEvent: event }), null, 2);
-  el.classList.remove('added-data');
-  setTimeout(function () {
-    return el.classList.add('added-data');
-  });
-  el.setAttribute('data-last-event-fired', event);
-}
-
-// Setup connections in here
-function setupListeners(store) {
-  document.querySelector('#inputBBB').addEventListener('keyup', function (e) {
-    store.publish('ADD_TEXT', { bbb: e.target.value });
-  });
-  document.querySelector('#buttonIncrement').addEventListener('click', function () {
-    store.publish('INCREMENT');
-  });
-  document.querySelector('#buttonDecrement').addEventListener('click', function () {
-    store.publish('DECREMENT');
-  });
-}
-
-// Entry function
-
-exports.default = function (initialState) {
-  var dom = document.querySelector('#connected2');
-  var store = (0, _createStore2.default)({
-    reducer: _reducer2.default,
-    initialState: initialState
-  }).connect(['ADD_TEXT', 'INCREMENT', 'DECREMENT'])(dom)(render);
-
-  setupListeners(store);
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };\n\nvar _createStore = __webpack_require__(0);\n\nvar _createStore2 = _interopRequireDefault(_createStore);\n\nvar _reducer = __webpack_require__(7);\n\nvar _reducer2 = _interopRequireDefault(_reducer);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// Perform DOM alterations in here\nfunction render(obj, el, event) {\n  var codeBlockEl = el.querySelector('[data-id=code]');\n  var lastEventEl = el.querySelector('[data-id=last-event]');\n  var titleEl = el.querySelector('[data-id=title]');\n  var valueEl = el.querySelector('[data-id=value]');\n\n  lastEventEl.innerText = event;\n  titleEl.innerText = obj.title;\n  valueEl.innerText = obj.value;\n  codeBlockEl.innerText = JSON.stringify(_extends({}, obj, { lastEvent: event }), null, 2);\n}\n\nfunction addListeners(el, store) {\n  el.querySelector('[data-id=input]').addEventListener('keyup', function (e) {\n    store.publish('UPDATE_TITLE', { title: e.target.value });\n  });\n  el.querySelector('[data-id=button-increment]').addEventListener('click', function () {\n    return store.publish('INCREMENT');\n  });\n  el.querySelector('[data-id=button-decrement]').addEventListener('click', function () {\n    return store.publish('DECREMENT');\n  });\n}\n\n// Entry function\n\nexports.default = function (initialState) {\n  var el = document.querySelector('[data-id=componentB]');\n  var store = (0, _createStore2.default)({\n    reducer: _reducer2.default,\n    initialState: initialState\n  }).connect(['UPDATE_TITLE', 'INCREMENT', 'DECREMENT'])(el)(render);\n\n  addListeners(el, store);\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/components/componentB/index.js\n// module id = 6\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/components/componentB/index.js?");
 
 /***/ }),
 /* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-// Perform STATE manipulations in here
-exports.default = function () {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  var action = arguments[1];
-
-  switch (action.type) {
-    case 'ADD_TEXT':
-      return _extends({}, state, action.data);
-    case 'INCREMENT':
-      return _extends({}, state, { value: state.value + 1 });
-    case 'DECREMENT':
-      return _extends({}, state, { value: state.value - 1 });
-    default:
-      return _extends({}, state);
-  }
-};
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };\n\n// Perform STATE manipulations in here\nexports.default = function () {\n  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};\n  var action = arguments[1];\n\n  switch (action.type) {\n    case 'UPDATE_TITLE':\n      return _extends({}, state, action.data);\n    case 'INCREMENT':\n      return _extends({}, state, { value: state.value + 1 });\n    case 'DECREMENT':\n      return _extends({}, state, { value: state.value - 1 });\n    default:\n      return _extends({}, state);\n  }\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/components/componentB/reducer.js\n// module id = 7\n// module chunks = 0\n\n//# sourceURL=webpack:///./src/components/componentB/reducer.js?");
 
 /***/ })
 /******/ ]);
