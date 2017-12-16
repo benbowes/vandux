d71e42dafb551396b88467d2a538c6d2c8de404f
Ben Bowes <benbowes@factor5.com.au>
2017-12-16 16:14:40 +1100

    Add test for no mutations

--- src/lib/stateMachine__test__.js
+++ src/lib/stateMachine__test__.js
@@ -6,9 +6,7 @@ describe('stateMachine', () => {
   it('should setup with initialState and get state', () => {
     const initialState = {
       a: 1,
-      b: {
-        c: [3, 2, 1]
-      }
+      b: { c: [3, 2, 1] }
     };
     deepFreeze(initialState);
 
@@ -19,20 +17,26 @@ describe('stateMachine', () => {
   it('should be able to setState and get state', () => {
     const initialState = {
       a: 1,
-      b: {
-        c: [3, 2, 1]
-      }
+      b: { c: [3, 2, 1] }
     };
     deepFreeze(initialState);
 
     const sm = stateMachine(initialState);
     sm.setState({ a: 2, b: { c: 3 } });
 
-    expect(sm.state).to.eql({
-      a: 2,
-      b: {
-        c: 3
-      }
-    });
+    expect(sm.state).to.eql({ a: 2, b: { c: 3 } });
+  });
+
+  it('should not mutate state', () => {
+    const initialState = {
+      a: 1,
+      b: { c: [3, 2, 1] }
+    };
+    deepFreeze(initialState);
+
+    const sm = stateMachine(initialState);
+    sm.setState({ a: 2, b: { c: 3 } });
+
+    expect(initialState.b).to.eql({ c: [3, 2, 1] });
   });
 });
